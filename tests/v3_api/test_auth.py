a64ed2a4eb920bb3f7d725db10627e1993e1b580
carolyn <carolyn@rancher.com>
2018-08-23 23:01:28 -0700

    print error

--- tests/v3_api/test_auth.py
+++ tests/v3_api/test_auth.py
@@ -51,6 +51,13 @@ auth_setup_fname = \
 
 def test_access_control_required_set_access_mode_required():
     access_mode = "required"
+    str2 = CA_CERTIFICATE
+    str1 = "-----BEGIN CERTIFICATE-----\nMIIFqDCCA5CgAwIBAgIJANQm89Tn7o6IMA0GCSqGSIb3DQEBCwUAMGExCzAJBgNV\nBAYTAlVTMRMwEQYDVQQIDApDYWxpZm9ybmlhMRIwEAYDVQQHDAlDdXBlcnRpbm8x\nEjAQBgNVBAoMCVJhamFzaHJlZTEVMBMGA1UEAwwMUmFqYXNocmVlIENBMB4XDTE3\nMDcxMzIzMTkxMloXDTM3MDcwODIzMTkxMlowYTELMAkGA1UEBhMCVVMxEzARBgNV\nBAgMCkNhbGlmb3JuaWExEjAQBgNVBAcMCUN1cGVydGlubzESMBAGA1UECgwJUmFq\nYXNocmVlMRUwEwYDVQQDDAxSYWphc2hyZWUgQ0EwggIiMA0GCSqGSIb3DQEBAQUA\nA4ICDwAwggIKAoICAQC3Rl0uelJgmcGeB5+T0KTRKc2+CWkm/M2hHElDf3UJk2PX\n5VP03+5a2CxjiPr9Ivz91asyVuUKeP7khnpWub9ZtHZQKOAIk0eh9vlOMtz3hT8y\neby37Svb10+UyriSAT3wMqTmhRHUaVLtDJcvbJ4iBBImCMptKaQc73EZKbnsVibm\nZlb/GaoGQj0nPH+mpaylQ53wEb9cIWgOUvszm+macrFwj4cGF7wluRGqgJZSM9Bi\nRBIy9ufjlb0WqZ0mo/rjZv/NGrS9boSAr9FGHaWzPSJoV7wpZqQkUkZ5cqWbf0co\nx4v3lrUyPD55NEzKLDC+oJc651hJCwkdERfELRi/fKq8ARBao0KikyqRXnbADIA/\nzaQrAKzpNrA0elnK4gbj3lsJepC1AueRy/TBqDJLH42pXXPsz9o8Vo3H+9+67wOC\nHgPB/trgeIPi1Ji49yXng6ix30L1FBRcExrD9KYCxVJZhmbafK+UWci8IBL0SKNG\nAGCkUgIz9J+DkLc4g7txfNdD9+N8sn+/h1xJ1xLUWvBLSvuYCLD+bQF8fWEHEZM1\nVjqBaW9wU/p2cbyfRVMXjhlRM4J7uZBiQpVbK8YUql7svFWrHH2EhcLxWjD/KnOt\niv9hjxIsPSqR4zYI8r0Xt/117GpdbyehF70GHxHl3h7jL6pwculjIeAk0lVEuQID\nAQABo2MwYTAdBgNVHQ4EFgQUoElq36seNa5+E7f4r/H4bzJj50AwHwYDVR0jBBgw\nFoAUoElq36seNa5+E7f4r/H4bzJj50AwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8B\nAf8EBAMCAYYwDQYJKoZIhvcNAQELBQADggIBAJWvMwn2nJvsuqTyV2xk9AGWLI95\ndqLhUZKjoFu0c3GQ6gSxS2g1jeZqbZWw9Juz7IJNw6AXp+cWP96v/EK2hdM1FW3v\ny8ZGjRUoXemxsiTOKTqXGO+v06gLWMYDKgr/cYotwyrKZ+PublMCDfeq9Y1jmw5A\nntu/sLi/CjzU4hwkJ+aIGFRP0CGXbBjjknHgW50DyyX64nWF7MBXXD1aHp3MlmSz\n/QON9qKrqUhWYUKdbGxv/NsQfwoNFL3MVsRokjmjZeLKRISODh8cEHpuuuKyP204\nhsitC84pE7icHbo1YZJMqCKr1QBCBnu+CO54kqIyY6fqc/vaSL4AD4NUBZDJLSXI\nzfaQZFCLbNFBAEZPsjv3iLbt7XvwsyqfPL+1H6UHcXCzzgfEMB+JztK31VRnlkHZ\nSy1VqqMiXhTyv9KzBt8uEb1CM/2PUQauKyAMWBKxrNfLrg+TNylPw8A4tFafKVie\nNfjBJSMQGzF8ftoD4WeADWK7dbifuqyyLQTLkI3XSQAl7slaax+FKTalnj674Hlr\nvR4cWUYXVljGOnQO8xSszJ11uTnXH53ZDyfz0VGi92zKiMJxD9SttoMwFlGgJmaF\npKBovwe7rseLXaRunDvNS/JJP8YVZxUEg86HuXO34f4lR9SlXK3cjeLiEYcP35uk\ndQ7yXAP0RthRkuac\n-----END CERTIFICATE-----",
+    print str2
+    print str1
+
+    if str1 == str2:
+        print "yes!!!!!!!"
     validate_access_control_set_access_mode(access_mode)
 
 
@@ -542,6 +549,7 @@ def enable_ad(username, token, expected_status=200):
       "username": username,
       "password": PASSWORD
     }, verify=False, headers=headers)
+    print r.json
     assert r.status_code == expected_status
     print "Enable ActiveDirectory request for " + username + " " + str(expected_status)
 
@@ -596,6 +604,7 @@ def enable_ad_nestedgroups(username, token, expected_status=200):
       "username": username,
       "password": PASSWORD
     }, verify=False, headers=headers)
+    print r.json
     assert r.status_code == expected_status
     print "Enable ActiveDirectory nestedgroup request for " + username + " " + str(expected_status)
 
